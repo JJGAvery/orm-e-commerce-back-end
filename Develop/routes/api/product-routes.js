a0ad03e503640c0029408ab47698a902873748e7
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-04-23 18:34:15 -0600

    added foreign keys to index.js products and categories. set up fields and rules for product models under product.js, categories under category.js, productTag under productTag.js, and tags under tag.js. finished the unfinished routes in product-routes.js, tag-routes.js and category-routes.js under api folder. npm run seeds to get all seeds to display under categories, tags and products.

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,18 +1,58 @@
+// The `/api/products` endpoint
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      res.status(500).json(err);
+    }
+    );
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+    ],
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    }
+    )
+    .catch((err) => {
+      res.status(500).json(err);
+    }
+    );
 });
 
 // create new product
@@ -25,9 +65,10 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
+ // if there's product tags, we need to create pairings to bulk create in the ProductTag model
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -47,9 +88,9 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+
+// update product data
 router.put('/:id', (req, res) => {
-  // update product data
   Product.update(req.body, {
     where: {
       id: req.params.id,
@@ -89,8 +130,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    }
+    )
+    .catch((err) => {
+      res.status(500).json(err);
+    }
+    );
 });
 
 module.exports = router;
